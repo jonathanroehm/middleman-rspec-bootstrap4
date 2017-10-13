89ee207c17ad6cdef3596f4963c554e0b3a1f96f
Jonathan Roehm <jonroehm@gmail.com>
2017-10-13 07:47:39 -0400

    converts application.js to application.js.coffee for coffee synax

--- source/javascripts/application.js
+++
@@ -1,9 +0,0 @@
-//= require jquery
-//= require popper
-//= require bootstrap
-
-// App
-//= require_self
-//= require_directory ./
-
-window.App = {}
---
+++ source/javascripts/application.js.coffee
@@ -0,0 +1,9 @@
+#= require jquery
+#= require popper
+#= require bootstrap
+
+# App
+#= require_self
+#= require_directory ./
+
+window.App = {}
